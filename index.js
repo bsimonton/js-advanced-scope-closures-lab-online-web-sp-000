7af213060cab65d836161b74602bda1107973070
Brian Simonton <bsimonton78@gmail.com>
2019-07-26 22:08:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function produceTipCalculator(tip){
+  
+  return function(fare){
+    
+    return tip * fare;
+  };
+  
+  
+}
+
+
+function createDriver(){
+  let driverID = 0 ;
+  
+  return class {
+    constructor(name){
+      this.id = ++driverID;
+      this.name = name;
+    }
+    
+  };
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
